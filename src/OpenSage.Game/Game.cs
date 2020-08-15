b2da2a3d780d940ad6c937390b9da7bf932a0056
Daan Meijer <daan@studioseptember.nl>
2020-08-16 01:55:53 +0200

    huge rework of the SkirmishManager: separated client and host, host can change options now, and client will see it!

--- src/OpenSage.Game/Game.cs
+++ src/OpenSage.Game/Game.cs
@@ -68,7 +68,7 @@ namespace OpenSage
 
         public InputMessageBuffer InputMessageBuffer { get; }
         
-        public SkirmishManager SkirmishManager { get; }
+        public SkirmishManager SkirmishManager { get; set; }
 
         //TODO: this will be part of SkirmishManager after merging litenetlib PR
         public MapCache CurrentMap { get; private set; }
@@ -473,8 +473,7 @@ namespace OpenSage
 
                 _mapTimer.Reset();
 
-                LobbyManager = new LobbyManager(this);
-                SkirmishManager = new SkirmishManager(this);                
+                LobbyManager = new LobbyManager(this);                
 
                 IsRunning = true;
                 IsLogicRunning = true;
@@ -814,7 +813,7 @@ namespace OpenSage
             Scene3D?.LocalLogicTick(MapTime, tickT);
 
             // TODO: do this properly (this is a hack to call StartMultiplayerGame on the correct thread)
-            if (SkirmishManager.SkirmishGame?.ReadyToStart ?? false)
+            if (SkirmishManager?.SkirmishGame?.ReadyToStart ?? false)
             {
                 SkirmishManager.SkirmishGame.ReadyToStart = false;
 
